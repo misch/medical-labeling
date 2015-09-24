8863c6bb6e6c8581ee25173a8defc1e73f22b346
Michèle Wyss <michele_wyss@students.unibe.ch>
2015-09-24 15:45:28 +0200

    implemented video to frames functionality in own function.

--- matlab-code/script.m
+++ matlab-code/script.m
@@ -1,24 +1,15 @@
-%% install VideoUtils Toolbox
-run 'C:\Program Files\MATLAB\R2010a\VideoUtils_v1_2_4\install.m';
 
-%% load video
+%% Define dataset and folders where to find them.
 dataset = 2;
-video_filename = ['../data/Training/Dataset',num2str(dataset),'/Video.avi'];
 
-vp = VideoPlayer(video_filename);
-% Todo: for a MATLAB newer than R2010a, use VideoReader!
-% vidObj = VideoReader(video_filename);
+dataset_folder = ['../data/Training/Dataset',num2str(dataset),'/'];
 
-%% Store frames as images
-% frames_dir = '../frames/';
-% start_frame = 0;
-% frames = read(vidObj);
-% 
-% for i = 1:size(frames,4)
-%    frameNr = sprintf('%03d',i+start_frame-1);
-%    filename = [frames_dir,'frame_',frameNr,'.png'];
-%    imwrite(frames(:,:,:,i),filename);
-% end
+video_filename = [dataset_folder,'Video.avi'];
+frames_dir = [dataset_folder,'input-frames/']
+
+%% Store video frames to .png images
+videoToFrame(video_filename, frames_dir);
+
 
 %% Collect mouse positions (simulating eye-tracking data)
 % Follow the thing you wanna track and click to update recorded mouse
---
+++ matlab-code/videoToFrames.m
@@ -0,0 +1,26 @@
+function videoToFrames(video_filename, frames_dir)
+% This script writes the single frames of a video into a .png images.
+% You have to set the variables 'video_filename' and 'frames_dir'
+% correctly.
+
+%% install VideoUtils Toolbox
+run 'C:\Program Files\MATLAB\R2010a\VideoUtils_v1_2_4\install.m';
+
+%% load video
+vp = VideoPlayer(video_filename);
+% Todo: for a MATLAB newer than R2010a, use VideoReader!
+% vidObj = VideoReader(video_filename);
+
+%% Store frames as images
+
+num_frames = vp.NumFrames;
+start_frame = 1;
+h = waitbar(0,'Initializing waitbar...');
+for i = 1:num_frames
+    frameNr = sprintf('%05d',i+start_frame-1)
+    filename = [frames_dir,'frame_',frameNr,'.png'];
+    frame = vp.Frame;
+    imwrite(frame,filename);
+    vp.nextFrame;
+    waitbar(i/num_frames,h,sprintf('%d / %d...',i, num_frames));
+end
