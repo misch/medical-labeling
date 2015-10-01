448c360ca531205f1fcd4bea31e71a119bd2ab9f
Michèle Wyss <michele_wyss@students.unibe.ch>
2015-10-01 11:47:53 +0200

    Splitting of data into training and test set.

--- matlab-code/script_classify.m
+++ matlab-code/script_classify.m
@@ -8,13 +8,24 @@ load([dataset_folder, 'processed_ROIs']);
 
 normalized_data = normalizeData(processed_ROIs);
 
-model = svmtrain(labels, normalized_data);
+%% Randomly select train and test data
+testPercent = 30;
 
-%%
-testing_label_vector = labels(end-5:end);
-testing_instance_matrix = normalized_data(end-5:end,:);
+test_indices = rand(1,size(normalized_data,1)) <= testPercent/100;
+train_indices = ~test_indices;
 
-[predicted_label, accuracy, decision_values] = svmpredict(testing_label_vector, testing_instance_matrix, model)
+train_data = normalized_data(train_indices,:);
+train_labels = labels(train_indices);
 
-% todo: separation of training and test data, check options for svmtrain,
-% plot ROC- and PR-curves, ...
+test_data = normalized_data(test_indices,:);
+test_labels = labels(test_indices);
+
+%% Train SVM
+
+model = svmtrain(train_labels, train_data);
+
+%% Test SVM
+[predicted_label, accuracy, decision_values] = svmpredict(test_labels, test_data, model);
+
+%% Plot ROC- and PR-curves
+% todo
