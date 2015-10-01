91ff812717c06d1244c3a0e9762beeb7699b2b63
Michèle Wyss <michele_wyss@students.unibe.ch>
2015-10-01 10:18:45 +0200

    added code for testing. todo: reasonable separation from training and test data.

--- matlab-code/script_classify.m
+++ matlab-code/script_classify.m
@@ -10,5 +10,11 @@ normalized_data = normalizeData(processed_ROIs);
 
 model = svmtrain(labels, normalized_data);
 
+%%
+testing_label_vector = labels(end-5:end);
+testing_instance_matrix = normalized_data(end-5:end,:);
+
+[predicted_label, accuracy, decision_values] = svmpredict(testing_label_vector, testing_instance_matrix, model)
+
 % todo: separation of training and test data, check options for svmtrain,
 % plot ROC- and PR-curves, ...
