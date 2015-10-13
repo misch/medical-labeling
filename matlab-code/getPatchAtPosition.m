90243f2246cd3f6347b1fde187965ba55adf1ffb
Michèle Wyss <michele_wyss@students.unibe.ch>
2015-10-13 16:01:54 +0200

    specified documentation of getPatchAtPosition

--- matlab-code/getPatchAtPosition.m
+++ matlab-code/getPatchAtPosition.m
@@ -1,5 +1,10 @@
 function [ROI] = getPatchAtPosition(image, position)
-% This function returns a 128x128x3 image patch around the given position.
+% This function returns a 128x128xQ patch around the given position, where
+% N is the number of images.
+%
+% Parameter:
+%   image - an MxNxQ matrix containing Q MxN images
+%   position - a two-dimensional vector containing x and y positions
 
 y_pos = round(position(1));
 x_pos = round(position(2));
