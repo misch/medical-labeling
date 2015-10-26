a8839bf61b05eb3c43c8f7e3f2f200e9041cfa48
Michèle Wyss <michele_wyss@students.unibe.ch>
2015-10-26 09:16:16 +0100

    normalize test and training data together, remove some zombie code
    Results still didn't improve...

--- matlab-code/script_classify.m
+++ matlab-code/script_classify.m
@@ -7,24 +7,6 @@ frames_dir = [dataset_folder,'input-frames/'];
 ground_truth_dir = [dataset_folder,'ground_truth-frames/'];
 
 load([dataset_folder, 'processed_ROIs']);
-[normalized_data, mu, sigma] = normalizeData(processed_ROIs);
-
-% processed_positives = normalized_data(labels == 1,:);
-
-%% Randomly select train and test data
-% testPercent = 20;
-% 
-% test_indices = rand(1,size(normalized_data,1)) <= testPercent/100;
-% train_indices = ~test_indices;
-% 
-% train_data = processed_positives;
-% train_labels = labels(labels == 1);
-
-% test_data = normalized_data(test_indices,:);
-% test_labels = labels(test_indices);
-
-train_data = normalized_data;
-train_labels = labels;
 
 
 %% get or create test set
@@ -47,38 +29,59 @@ else
     test_data = reshape(test_data,size(test_labels,1),[]);
 end
 
-test_data = normalizeData(test_data);
+%% normalize data (test and training set together!)
+to_normalize = cat(1,processed_ROIs,test_data);
+normalized_data = normalizeData(to_normalize);
+
 
 %% Train SVM
+
+train_data = normalized_data(1:size(processed_ROIs,1),:); % only positives to train one-class svm
+train_labels = labels;
+% To shorten computation time...
+% train_percentage = 20;
+% train_indices = find(rand(1,size(train_data,1)) <= train_percentage/100);
+% train_data = train_data(train_indices,:);
+% train_labels = train_labels(train_indices);
+
+train_data = train_data(find(train_labels==1),:);
+train_labels = train_labels(train_labels==1);
+
 disp('Train SVM classifier...');
-% model = libsvmtrain(train_labels, train_data,'-t 2 -g 0.0625 -c 0.05');
+% model = libsvmtrain(train_labels,train_data,'-t 2 -g 0.5 -c 5 -h 0');
+model = libsvmtrain(train_labels,train_data,'-s 2 -n 0.4 -c 2');
 
-bestcv = 0;
-for log2c = -3:3,
-  for log2g = -4:4,
-    cmd = ['-v 10 -c ', num2str(2^log2c), ' -g ', num2str(2^log2g)];
-    cv = svmtrain(train_labels, train_data, cmd);
-    if (cv >= bestcv),
-      bestcv = cv; bestc = 2^log2c; bestg = 2^log2g;
-    end
-    fprintf('%g %g %g (best c=%g, g=%g, rate=%g)\n', log2c, log2g, cv, bestc, bestg, bestcv);
-  end
-end
-
-
-
-%% Show some positives
-% positives = (test_labels == 1);
-% positives = test_data(positives,:);
-% 
-% figure();
-% for i = 1:25
-%     subplot(5,5,i);
-%     imshow(reshape(positives(i+10000,:),32,[])+0.5);
+% bestacc = 0;
+% for log2c = -3:3,
+%   for log2n = -8:0,
+% %     cmd = ['-v 4 -c ', num2str(2^log2c), ' -g ', num2str(2^log2g)];
+%     cmd = ['-s 2 -n ', num2str(2^log2n),' -c ',num2str(2^log2c)];
+%     model = libsvmtrain(train_labels, train_data, cmd);
+%     [predicted_labels, accuracy, decision_values] = libsvmpredict(test_labels, test_data, model);
+%     if (accuracy >= bestacc),
+%       bestacc = accuracy; bestc = 2^log2c; bestn = 2^log2n;
+%     end
+% %     fprintf('%g %g %g (best c=%g, g=%g, rate=%g)\n', log2c, log2g, cv, bestc, bestg, bestcv);
+%     fprintf('c=%g n=%g acc=%g (best c=%g, n=%g, rate=%g)\n', 2^log2c, 2^log2n, accuracy, bestc, bestn, bestacc);
+%   end
 % end
+
+
+
+
 
 %%
 disp('Test SVM classifier...');
+
+% To reduce computation time
+% test_percentage = 1;
+% test_indices = find(rand(1,size(test_data,1)) <= test_percentage/100);
+% 
+% test_data = test_data(test_indices,:);
+% test_labels = test_labels(test_indices);
+
+test_data = normalized_data(size(processed_ROIs,1)+1:size(processed_ROIs,1)+size(test_data,1),:);
+
 [predicted_label, accuracy, decision_values] = libsvmpredict(test_labels, test_data, model);
 
 
@@ -91,31 +94,31 @@ positives = sorted_test_labels == 1;
 
 % Precision-Recall-curve (PR)
 
-% Precision: TP / (TP + FP) = TP/#{all predicted positives}
-% What fraction of the predicted positives are actually positive?
-precision = cumsum(positives)./(1:Ntest)'; % is less than 1 if the number of (actual) positives are found (hopefully, recall is 1 then)
+    % Precision: TP / (TP + FP) = TP/#{all predicted positives}
+    % What fraction of the predicted positives are actually positive?
+    precision = cumsum(positives)./(1:Ntest)'; % is less than 1 if the number of (actual) positives are found (hopefully, recall is 1 then)
 
-% Recall = TPR (true positive rate) = TP / (TP + FN) = TP/#{positives} 
-% How many of the positive test labels are actually found?
-recall = cumsum(positives)/sum(positives); % is less than 1 if not yet all points are considered (hopefully, precision is 1 then)
+    % Recall = TPR (true positive rate) = TP / (TP + FN) = TP/#{positives} 
+    % How many of the positive test labels are actually found?
+    recall = cumsum(positives)/sum(positives); % is less than 1 if not yet all points are considered (hopefully, precision is 1 then)
 
-figure(4);
-plot(recall,precision);
-axis( [0 1 0 1] );
-title('precision-recall curve');
-xlabel('Recall');
-ylabel('Precision');
+    figure;
+    plot(recall,precision);
+    axis( [0 1 0 1] );
+    title('precision-recall curve');
+    xlabel('Recall');
+    ylabel('Precision');
 
 % Receiver Operating Characteristic curve (ROC)
 % FPR = FP / (FP + TN) = FP/#{negatives}
-false_positive_rate = cumsum(~positives) ./ sum(~positives)';
+    false_positive_rate = cumsum(~positives) ./ sum(~positives)';
 
-figure(5);
-plot(false_positive_rate,recall);
-axis( [0 1 0 1] );
-title('ROC curve');
-xlabel('False Positive Rate');
-ylabel('True Positive Rate');
+    figure;
+    plot(false_positive_rate,recall);
+    axis( [0 1 0 1] );
+    title('ROC curve');
+    xlabel('False Positive Rate');
+    ylabel('True Positive Rate');
 
 % visualize decision_values
 file_names = dir([frames_dir, '*.png']);
@@ -124,10 +127,19 @@ ref_frame = imread([frames_dir,file_names(1).name]);
 height = size(ref_frame,1) - 127;
 width = size(ref_frame,2) - 127;
 
-figure(6);
-% imshow(reshape(decision_values, height, []), [min(decision_values), max(decision_values)]);
-% title(['max: ',num2str(max(decision_values)),' / min: ', num2str(min(decision_values))]);
+    % Heat map
+    figure;
+    colormap('hot');   % set colormap
+    final_image_decisions = reshape(decision_values, height, []);
+    imagesc(final_image_decisions);        % draw image and scale colormap to values range
+    colorbar;          % show color scale
 
- colormap('hot');   % set colormap
- imagesc(reshape(decision_values, height, []));        % draw image and scale colormap to values range
- colorbar;          % show color scale
+    % Binary decisions
+    figure;
+    imshow(final_image_decisions>=0);
+
+    % Ground truth
+    figure;
+    imshow(reshape(test_labels,height,[]));
+ 
+ 
