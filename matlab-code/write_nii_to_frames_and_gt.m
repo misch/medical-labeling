e8933b1f3c4f5345437796920921684a0a4b96d8
Michèle Wyss <michele_wyss@students.unibe.ch>
2015-11-03 20:44:31 +0100

    resizing easier changeable for .nii data sets

--- matlab-code/write_nii_to_frames_and_gt.m
+++ matlab-code/write_nii_to_frames_and_gt.m
@@ -1,33 +1,42 @@
 % Write .nii file to frames
 addpath('../NIfTI_20140122/');
 
-dataset = 7;
+dataset = 8;
 [dataset_folder, frames_dir] = getDatasetDetails(dataset);
 
-patient_file = [dataset_folder,'Sub04R/Patient_T2.nii'];
+% patient_file = [dataset_folder,'Sub04R/Patient_T2.nii'];
+patient_file = [dataset_folder, 'Labyrinth.nii'];
 data = load_untouch_nii(patient_file);
-img = im2double(data.img);
+
+img = double(data.img);
+img = img - min(img(:));
 img = img./max(img(:));
 
-ground_truth_file = [dataset_folder,'Sub04R/GTVT2.nii'];
+% ground_truth_file = [dataset_folder,'Sub04R/GTVT2.nii'];
+ground_truth_file = [dataset_folder, 'Labyrinth-mask.nii'];
 gt_data = load_untouch_nii(ground_truth_file);
 mask = double(gt_data.img);
 
 
 mkdir(frames_dir);
 
-start_frame = 12;
+start_frame = 1;
+resize_scale = 2;
 
 for i = start_frame:size(img,3)
     frameNr = sprintf('%05d', i-start_frame+1);
     filename = [frames_dir,'frame_',frameNr,'.png'];
-    imwrite(imresize(img(:,:,i),4,'nearest'),filename);
+    current_img = img(:,:,i);
+    current_img = imresize(current_img,resize_scale,'nearest');
+    imwrite(current_img,filename);
 end
 
-gt_folder = [dataset_folder,'ground-truth-frames/'];
+gt_folder = [dataset_folder,'ground_truth-frames/'];
 mkdir(gt_folder);
 for i = 1:size(mask,3)
     frameNr = sprintf('%05d', i-start_frame+1);
     filename = [gt_folder,'frame_',frameNr,'.png'];
-    imwrite(imresize(mask(:,:,i),4,'nearest'),filename);
+    current_gt = mask(:,:,i);
+    current_gt = imresize(current_gt,resize_scale,'nearest');
+    imwrite(current_gt,filename);
 end
