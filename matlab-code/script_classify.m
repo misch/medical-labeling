9bc2c262dbaf7e172bb708378a06ba89be20aa41
Michèle Wyss <michele_wyss@students.unibe.ch>
2015-11-05 14:18:03 +0100

    can now use gradient boost classifier
    results seem more promising...

--- matlab-code/script_classify.m
+++ matlab-code/script_classify.m
@@ -1,7 +1,5 @@
 %% Define data paths and actions
-addpath('../libsvm-3.20/libsvm-3.20/matlab/')
-
-dataset = 8;
+dataset = 5;
 [dataset_folder, frames_dir, ~ , frame_height, frame_width, num_frames] = getDatasetDetails(dataset);
 
 if (exist([dataset_folder,'ground_truth-frames'],'dir') > 0)
@@ -16,7 +14,7 @@ load([dataset_folder, 'processed_ROIs']);
 %% get or create test set
 frame_percentage = 0.1; % rough percentage of #test-frames
 
-create_new_test_set = true;
+create_new_test_set = false;
 
 if create_new_test_set
     [test_data, test_labels] = createTestData(frames_dir,frame_percentage,ground_truth_dir);
@@ -37,49 +35,70 @@ end
 %% normalize data (test and training set together!)
 to_normalize = cat(1,processed_ROIs,test_data);
 normalized_data = normalizeData(to_normalize);
+clear to_normalize;
 
 
-%% Train SVM
+%% Train Classifier
 train_data = normalized_data(1:size(processed_ROIs,1),:);
 train_labels = labels;
 
-cross_validation = true;
+classifier = 'grad_boost' % 'svm' or 'grad_boost'
 
-if (cross_validation) % Perform cross-validation
-    disp('Start cross-validation...');
-    [train_data, train_labels] = getFiftyFiftySamples(train_data,train_labels);
+if strcmp(classifier,'svm')
+    addpath('../libsvm-3.20/libsvm-3.20/matlab/')
+    cross_validation = false;
 
-    bestacc = 0;
-    for log2c = -3:3,
-      for log2g = -4:2,
-        cmd = ['-v 5 -c ', num2str(2^log2c), ' -g ', num2str(2^log2g)];
-        cv = libsvmtrain(train_labels, train_data, cmd);
-        if (cv >= bestacc),
-          bestacc = cv; bestc = 2^log2c; bestg = 2^log2g;
+    if (cross_validation) % Perform cross-validation
+        disp('Start cross-validation...');
+        [train_data, train_labels] = getFiftyFiftySamples(train_data,train_labels);
+
+        bestacc = 0;
+        for log2c = -3:3,
+          for log2g = -4:2,
+            cmd = ['-v 5 -c ', num2str(2^log2c), ' -g ', num2str(2^log2g)];
+            cv = libsvmtrain(train_labels, train_data, cmd);
+            if (cv >= bestacc),
+              bestacc = cv; bestc = 2^log2c; bestg = 2^log2g;
+            end
+            fprintf('%g %g %g (best c=%g, g=%g, rate=%g)\n', log2c, log2g, cv, bestc, bestg, bestacc);
+          end
         end
-        fprintf('%g %g %g (best c=%g, g=%g, rate=%g)\n', log2c, log2g, cv, bestc, bestg, bestacc);
-      end
+    else % Perform real training
+        disp('Train SVM classifier...');
+        model = libsvmtrain(train_labels,train_data,'-t 2 -g 0.0625 -c 8');
+        % model = libsvmtrain(train_labels,train_data,'-s 2 -n 0.001 -c 0.2');
+        % model = libsvmtrain(train_labels,train_data,'-s 2');
     end
-else % Perform real training
-    disp('Train SVM classifier...');
-    model = libsvmtrain(train_labels,train_data,'-t 2 -g 0.0625 -c 8');
-    % model = libsvmtrain(train_labels,train_data,'-s 2 -n 0.001 -c 0.2');
-    % model = libsvmtrain(train_labels,train_data,'-s 2');
+elseif strcmp(classifier,'grad_boost')
+   addpath('../sqb-0.1/build/');
+   options = struct(   'loss', 'exploss',...
+                    'shrinkageFactor', 0.1,...
+                    'subsamplingFactor', 0.5,...
+                    'maxTreeDepth', uint32(2),...
+                    'disableLineSearch', uint32(0),...
+                    'mtry', uint32(ceil(sqrt(size(train_data,2)))));
+    disp('Train gradient boost classifier...');
+    model = SQBMatrixTrain( single(train_data), train_labels, uint32(10000), options);
+else
+    disp('SVM and Gradient Boost are currently the only available classifiers.')
 end
 
-
 %% Test SVM
-disp('Test SVM classifier...');
+disp('Test Cassifier...');
 
 test_data = normalized_data(size(processed_ROIs,1)+1:size(processed_ROIs,1)+size(test_data,1),:);
 
-[predicted_label, accuracy, decision_values] = libsvmpredict(test_labels, test_data, model);
+if strcmp(classifier,'svm')
+    [predicted_label, accuracy, scores] = libsvmpredict(test_labels, test_data, model);
+elseif strcmp(classifier,'grad_boost')
+    scores = SQBMatrixPredict(model, single(test_data)); 
+end
 
 
 %% Evaluation 
 disp('Show performance measures...');
 Ntest = size(test_data,1);
-[sorted_scores,idx] = sort(decision_values,'descend');
+[sorted_scores,idx] = sort(scores,'descend');
 sorted_test_labels = test_labels(idx);
 positives = sorted_test_labels == 1;
 
@@ -121,7 +140,7 @@ width = frame_width - 127;
     % Heat map
     figure;
     colormap('hot');   % set colormap
-    final_image_decisions = reshape(decision_values, height, []);
+    final_image_decisions = reshape(scores, height, []);
     imagesc(final_image_decisions);        % draw image and scale colormap to values range
     colorbar;          % show color scale
 
