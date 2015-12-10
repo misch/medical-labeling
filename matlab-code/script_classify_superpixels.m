8491276ff7b6f6ee503a23b6371d86a1503e5132
Michele Wyss <michele_wyss@students.unibe.ch>
2015-12-10 12:52:20 +0000

    removed some old code from the classify-script
    script will be removed eventually - might still need parts to build the
    patch testing function

--- matlab-code/script_classify_superpixels.m
+++ matlab-code/script_classify_superpixels.m
@@ -9,82 +9,39 @@ else
 end
 
 
-load([dataset_folder, 'superpixelFeatures']);
+load([dataset_folder, 'trainingSuperpixels']);
 
 
 %% get or create test set
-frame_percentage = 101; % rough percentage of #test-frames
 
-create_new_test_set = true;
-
-if create_new_test_set
-    [test_data, super_img] = createTestData_superpixels(dataset_folder,frames_dir,frame_percentage);
-    test_labels = zeros(size(test_data,1),1);
-else
-%     frame_no = '00428'; % for dataset 5
-%     frame_no = '00642'; % for dataset 2
-%     data_id = fopen([dataset_folder,'test_data_frame_',frame_no,'.dat']);
+% ===============================================================================================
+% Is that code still needed?
+% ===============================================================================================
+%{
     frame_no = '00189'; % for dataset 8
     data_id = fopen([dataset_folder,'test-data/superpixels/','test_data_frame_',frame_no,'.dat']);
     test_data = fread(data_id,'double');
     fclose(data_id);
     
-%     labels_id = fopen([dataset_folder,'test_labels_frame_',frame_no,'.dat']);
     labels_id = fopen([dataset_folder,'test-data/','test_labels_frame_',frame_no,'.dat']);
     test_labels = fread(labels_id,'double');
     fclose(labels_id);
     
     test_data = reshape(test_data,size(test_labels,1),[]);
-end
+
+    % this code loads some particular test-data of one frame; this
+    % test-data consists of descriptors and information about ground truth
+    % (in case of superpixels, just information to backproject stuff back
+    % to the single pixels)
+%}
+% ===============================================================================================    
 
 
-%% Train Classifier
-train_data = normalized_data(1:size(processed_ROIs,1),:);
-% train_data = processed_ROIs;
-train_labels = labels;
-
-classifier = 'grad_boost' % 'svm' or 'grad_boost'
-
-if strcmp(classifier,'svm')
-    addpath('../libsvm-3.20/libsvm-3.20/matlab/')
-    cross_validation = false;
-
-    if (cross_validation) % Perform cross-validation
-        disp('Start cross-validation...');
-        [train_data, train_labels] = getFiftyFiftySamples(train_data,train_labels);
-
-        bestacc = 0;
-        for log2c = -3:3,
-          for log2g = -4:2,
-            cmd = ['-v 5 -c ', num2str(2^log2c), ' -g ', num2str(2^log2g)];
-            cv = libsvmtrain(train_labels, train_data, cmd);
-            if (cv >= bestacc),
-              bestacc = cv; bestc = 2^log2c; bestg = 2^log2g;
-            end
-            fprintf('%g %g %g (best c=%g, g=%g, rate=%g)\n', log2c, log2g, cv, bestc, bestg, bestacc);
-          end
-        end
-    else % Perform real training
-        disp('Train SVM classifier...');
-        model = libsvmtrain(train_labels,train_data,'-t 2 -g 0.0625 -c 8');
-    end
-elseif strcmp(classifier,'grad_boost')
-   addpath('../sqb-0.1/build/');
-   options = struct(   'loss', 'exploss',...
-                    'shrinkageFactor', 0.1,...
-                    'subsamplingFactor', 0.5,...
-                    'maxTreeDepth', uint32(2),...
-                    'disableLineSearch', uint32(0),...
-                    'mtry', uint32(ceil(sqrt(size(train_data,2)))));
-    disp('Train gradient boost classifier...');
-    model = SQBMatrixTrain( single(train_data), train_labels, uint32(10000), options);
-else
-    disp('SVM and Gradient Boost are currently the only available classifiers.')
-end
 
 %% Test SVM
 disp('Test Classifier...');
 
+% todo: get that from prepared descriptors!
 test_data = normalized_data(size(processed_ROIs,1)+1:size(processed_ROIs,1)+size(test_labels,1),:);
 
 
@@ -107,10 +64,6 @@ for i = 1:(max(super_img(:)))
 end
 
 projected_scores = projected_img(:);
-
-Ntest = size(projected_scores,1);
-[sorted_scores,idx] = sort(projected_scores,'descend');
-
 
 
 gt = getGrayScaleImage([ground_truth_dir,'frame_',frame_no,'.png']);
@@ -169,5 +122,3 @@ positives = sorted_test_labels == 1;
     % Ground truth
     figure;
     imshow(gt);
- 
- 
