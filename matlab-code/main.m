5a7575133db6c753f70c82edde3b06ca9b3ad4ce
Michele Wyss <michele_wyss@students.unibe.ch>
2016-01-09 10:36:30 +0000

    added some comments with used sizes and frames

--- matlab-code/main.m
+++ matlab-code/main.m
@@ -4,11 +4,18 @@
 
 dataset = 2;
 	
-assembleTrainingDataSuperpixels(dataset,'trainingSuperpixelsCoocc.mat');
-assembleTrainingDataPatches(dataset,'trainingPatches.mat');
+% assembleTrainingDataSuperpixels(dataset,'trainingSmallSuperpixelsCoocc.mat');
+% assembleTrainingDataPatches(dataset,'trainingPatches.mat');
 
 classifier = 'grad_boost';
-model = trainClassifier(dataset, classifier, 'trainingSuperpixels');
+model = trainClassifier(dataset, classifier, 'trainingSmallSuperpixelsCoocc');
 
+%%
 testSuperpixelClassifier(model, dataset, [105:50:655], classifier);
-% testPatchClassifier(model,dataset,[105:50:655], classifier);
+% testPatchClassifier(model,dataset,[139:10:229], classifier);
+
+% frames to test:
+% Dataset2: [105:50:655]
+% Dataset5: ???
+% Dataset7: [20:5:55]
+% Dataset8: [139:10:229]
--- matlab-code/testSuperpixels.m
+++ matlab-code/testSuperpixels.m
@@ -1,19 +1,22 @@
 % first: drag the corresponding image to the workspace
+% [filename, impath] = uigetfile('../data/*.png');
+% image = im2double(imread([impath,filename]));
+
 lab_image = image;
 
 if (size(image,3) == 3)
     lab_image = rgb2lab(image);
 end
 
-superpixel_size = round(min([size(image,1), size(image,2)]) / 11.5);
-super_img = getSuperPixels(single(lab_image), superpixel_size, 40);
+superpixel_size = round(min([size(image,1), size(image,2)]) / 16.5);
+super_img = getSuperPixels(single(lab_image), superpixel_size, 0.05);
 
 grad = imgradient(super_img);
 
 imshow(image + repmat(grad,1,1,size(image,3)));
 
 % used regularizer values:
-% Dataset2: 300
+% Dataset2: 300, big size: 11.5; small size = 16.5
 % Dataset5: 40
-% Dataset7: 0.05
-% Dataset8: 0.03
+% Dataset7: 0.05, big size: 11.5, small size= 16.5
+% Dataset8: 0.03, big size: 11.5, small size: 19.5
