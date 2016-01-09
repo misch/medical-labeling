a695a67524b6d3bb9d3f8d9eb0680cf72dbdd2b1
Michele Wyss <michele_wyss@students.unibe.ch>
2016-01-09 10:41:08 +0000

    added one more curve to plotting...
    todo: make it easier to compare more or less of the results (#13)

--- matlab-code/plotDifferentCurves.m
+++ matlab-code/plotDifferentCurves.m
@@ -16,6 +16,11 @@ load([data_path_supercoocc,filename]);
 
 hold on; plot(recall,precision,'r');
 
+disp('Choose small superpixel/coocc PR.mat');
+[filename, data_path_smallsupercoocc] = uigetfile([dataset_folder,'*.mat'])
+load([data_path_smallsupercoocc,filename]);
+
+hold on; plot(recall,precision,'Color', [0 0.5 0]);
 
 disp('Choose patch PR.mat');
 [filename, data_path_patch] = uigetfile([dataset_folder,'*.mat'])
@@ -26,7 +31,7 @@ axis( [0 1 0 1] );
 title('precision-recall curve');
 xlabel('Recall');
 ylabel('Precision');
-legend('superpixels (hist40, mean, var)','superpixels (hist10, mean, var, co-occurrence)','patches');
+legend('superpixels (hist40, mean, var)','superpixels (hist10, mean, var, co-occurrence)','small superpixels (hist10, mean, var, co-occ)','patches');
 
 %%
 filename = 'ROC.mat';
@@ -50,7 +55,18 @@ load([data_path_supercoocc, filename]);
 
 hold on; plot(false_positive_rate,recall,'r');
 
-if ~exist('data_path_supercoocc','var')
+
+if ~exist('data_path_smallsupercoocc','var')
+    disp('Choose small superpixel/coocc ROC.mat');
+    [filename, data_path_smallsupercoocc] = uigetfile([dataset_folder,'*.mat']);
+end
+
+load([data_path_smallsupercoocc, filename]);
+
+hold on; plot(false_positive_rate,recall,'Color', [0 0.5 0]);
+
+
+if ~exist('data_path_patch','var')
     disp('Choose patch ROC.mat');
     [filename, data_path_patch] = uigetfile([dataset_folder,'*.mat']);
 end
@@ -63,4 +79,4 @@ axis( [0 1 0 1] );
 title('ROC curve');
 xlabel('False Positive Rate');
 ylabel('True Positive Rate');
-legend('superpixels (hist40, mean, var)','superpixels (hist10, mean, var, co-occurrence)','patches');
+legend('superpixels (hist40, mean, var)','superpixels (hist10, mean, var, co-occurrence)','small superpixels (hist10, mean, var, co-occ)','patches');
