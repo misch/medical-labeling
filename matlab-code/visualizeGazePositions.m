a8ef679023a421e8be7a0dd9cfdfa35123050ade
Michèle Wyss <michele_wyss@students.unibe.ch>
2016-01-14 18:33:34 +0100

    more things visualized close #14
    key pressed or not, gaze on object or not

--- matlab-code/visualizeGazePositions.m
+++ matlab-code/visualizeGazePositions.m
@@ -1,4 +1,4 @@
-[dataset_folder, ~, ~, frame_height, frame_width] = getDatasetDetails(2);
+[dataset_folder, ~, ~, frame_height, frame_width] = getDatasetDetails(7);
 ground_truth_dir = [dataset_folder,'ground_truth-frames/'];
 gt_files = dir([ground_truth_dir, '*.png']);
 
@@ -28,35 +28,52 @@ for i = 1:length(gt_files)
 end
 
 cols = [zeros(size(framePositions(:,3),1),1), gt_vals*0.6, zeros(size(framePositions(:,3),1),1)];
-
+key_pressed = framePositions(:,3) > 0;
 %%
 figure;
 subplot(2,2,1);
 frameax = [1:length(framePositions(:,1))]';
-scatter(frameax, framePositions(:,1),[],cols);
+scatter(frameax(key_pressed), framePositions(key_pressed,1), [],cols(key_pressed,:,:), 'o'); hold on;
+scatter(frameax(~key_pressed), framePositions(~key_pressed,1), [],cols(~key_pressed,:,:), '+');
 axis([0 length(frameax) 0 frame_width]);
 title('horizontal movements (y axis)');
+
+% legend
+legendies(1) = plot(NaN,NaN,'ok'); % 'o' --> key pressed
+legendies(2) = plot(NaN,NaN,'+k'); % '+' --> key not pressed
+legendies(3) = plot(NaN,NaN,'s','MarkerFaceColor',[0,0.6,0]); % green --> on object
+legendies(4) = plot(NaN,NaN,'sk','MarkerFaceColor','k'); % black --> not on object
+legend(legendies,'key pressed','key not pressed','gaze on object','gaze not on object');
+
+
+
 
 subplot(2,2,3);
 plot(gradient(framePositions(:,1)));
 title('horizontal gradient (y axis)');
 
+
 subplot(2,2,2);
 frameax = [1:length(framePositions(:,2))]';
-scatter(frameax, framePositions(:,2),[],cols);
+scatter(frameax(key_pressed), framePositions(key_pressed,2), [],cols(key_pressed,:,:), 'o'); hold on;
+scatter(frameax(~key_pressed), framePositions(~key_pressed,2), [],cols(~key_pressed,:,:), '+');
 axis([0 length(frameax) 0 frame_height]);
 title('vertical movements (x axis)');
+
+legend(legendies,'key pressed','key not pressed','gaze on object','gaze not on object');
 
 subplot(2,2,4);
 plot(gradient(framePositions(:,2)));
 title('vertical gradient (x axis)');
 
-
+h = gcf;
+set(h,'PaperOrientation','landscape');
 %% 
 key_pressed = framePositions(:,3) > 0;
 figure; hold on;
 scatter(framePositions(key_pressed,1), framePositions(key_pressed,2), [],cols(key_pressed,:,:), 'o'); hold on;
 scatter(framePositions(~key_pressed,1), framePositions(~key_pressed,2),[],cols(~key_pressed,:,:),'+'); axis([0 frame_width 0 frame_height]);
+title('Dataset 7');
 
 % create plot legend
 legendies(1) = plot(NaN,NaN,'ok'); % 'o' --> key pressed
