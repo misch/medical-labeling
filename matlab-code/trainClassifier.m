d5c820a1952e2925674cd475140278ef776d0880
Michele Wyss <michele_wyss@students.unibe.ch>
2016-02-03 14:53:45 +0000

    merged trainClassifier

--- matlab-code/trainClassifier.m
+++ matlab-code/trainClassifier.m
@@ -33,20 +33,21 @@ if strcmp(classifier,'svm')
     else % Perform real training
         disp('Train SVM classifier...');
         [train_data, train_labels] = getFiftyFiftySamples(train_data,train_labels);
-        model = libsvmtrain(train_labels,train_data,'-t 2 -g 0.0625 -c 8');
+        model = libsvmtrain(train_labels,train_data,'-t 2 -g 0.0625 -c 10');
 %         model = fitcsvm(train_data, train_labels,'KernelFunction','RBF','KernelScale','auto');
     end
 elseif strcmp(classifier,'grad_boost')
    addpath('../sqb-0.1/build/');
    options = struct(   'loss', 'exploss',...
                     'shrinkageFactor', 0.1,...
-                    'subsamplingFactor', 0.5,...
-                    'maxTreeDepth', uint32(0),...
+                    'subsamplingFactor', 0.1,...
+                    'maxTreeDepth', uint32(2),...
                     'disableLineSearch', uint32(0),...
-                    'mtry', uint32(ceil(sqrt(size(train_data,2)))));
+                     'mtry',uint32(ceil(sqrt(size(train_data,2)))));
     disp('Train gradient boost classifier...');
-    model = SQBMatrixTrain( single(train_data), train_labels, uint32(2000), options);
+    model = SQBMatrixTrain( single(train_data), train_labels, uint32(1000), options);
 elseif strcmp(classifier,'pu_grad_boost')
+    
     addpath('./pugradboost/');
     
 %     distances = sqrt(sum((training_set.gaze_position - training_set.median_superpixel_pos).^2,2));
@@ -59,10 +60,11 @@ elseif strcmp(classifier,'pu_grad_boost')
     
     
     prob = exp(-distances/22);
+
 %     prob(train_labels == -1) = 0.001;
 %     prob(7:15) = 0.4;
     train_labels(train_labels == -1) = 0;
-    train_labels(7:15) = 0;
+%     train_labels(7:15) = 0;
 
     [model,~] = learnPuboost(train_data,train_labels, prob,200);
 else
