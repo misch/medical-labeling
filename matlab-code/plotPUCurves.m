97047012a9a91ce1bbfc172842a52db116ea4bbb
Michele Wyss <michele_wyss@students.unibe.ch>
2016-02-05 09:41:32 +0000

    median distance again instead of distance of median

--- matlab-code/plotPUCurves.m
+++ matlab-code/plotPUCurves.m
@@ -1,15 +1,30 @@
+res_fold = '../results/comparisons/Dataset2/';
     figure;
-    load('PR15');
-    plot(recall,precision);
-    load('PR28');
+    load([res_fold,'PRpu400']);
+    plot(recall, precision);
+    
+    load([res_fold,'PR_probSpatialColor_featureSuperpixelCoocc']);
     hold on; plot(recall,precision);
-    load('PR72');
-    hold on; plot(recall, precision);
+    
+    load([res_fold,'PRclassicalRS']);
+    hold on; plot(recall,precision);
+    
+    load([res_fold,'PRclassicalCBexploss']);
+    hold on; plot(recall,precision);
+    
+    load([res_fold,'PRclassicalCBsquaredloss']);
+    hold on; plot(recall,precision);
+    
     axis( [0 1 0 1] );
     title('precision-recall curve');
     xlabel('Recall');
     ylabel('Precision');
-    legend('p = exp^{-dist/15} (--> tolerance = 10.4)','p = exp^{-dist/28} (--> tolerance = 19.4)','p = exp^{-dist/72} (--> tolerance = 49.9)');
+%     legend('p = exp^{-dist/15} (--> tolerance = 10.4)','p = exp^{-dist/28} (--> tolerance = 19.4)','p = exp^{-dist/72} (--> tolerance = 49.9)');
+    legend( 'PU prob. based on spatial & feature distance, feat. = color-relations',...
+            'PU prob. based on spatial and color-relation distance, feat. = [hist10,mean,var,co-occ.]',...
+            'classical (RS implementation - exploss)',...
+            'classical (CB implementation exploss)',...
+            'classical (CB implementation squaredloss)');
 
     %%
     
@@ -18,14 +33,31 @@
 % FPR = FP / (FP + TN) = FP/#{negatives}
 
     figure;
-    load('ROC15');
+%     load('ROC300');
+%     hold on; plot(false_positive_rate,recall);
+
+    load([res_fold,'ROCpu400']);
     plot(false_positive_rate,recall);
-    load('ROC28');
+    
+    load([res_fold,'ROC_probSpatialColor_featureSuperpixelCoocc']);
     hold on; plot(false_positive_rate,recall);
-    load('ROC72');
+    
+    load([res_fold,'ROCclassicalRS']);
     hold on; plot(false_positive_rate,recall);
+    
+    load([res_fold,'ROCclassicalCBexploss']);
+    hold on; plot(false_positive_rate,recall);
+    
+    load([res_fold,'ROCclassicalCBsquaredloss']);
+    hold on; plot(false_positive_rate,recall);
+
     axis( [0 1 0 1] );
     title('ROC curve');
     xlabel('False Positive Rate');
     ylabel('True Positive Rate (Recall)');
-    legend('p = exp^{-dist/15} (--> tolerance = 10.4)','p = exp^{-dist/28} (--> tolerance = 19.4)','p = exp^{-dist/72} (--> tolerance = 49.9)');
+%     legend('p = exp^{-dist/15} (--> tolerance = 10.4)','p = exp^{-dist/28} (--> tolerance = 19.4)','p = exp^{-dist/72} (--> tolerance = 49.9)');
+    legend( 'PU prob. based on spatial & feature distance, feat. = color-relations',...
+            'PU prob. based on spatial and color-relation distance, feat. = [hist10,mean,var,co-occ.]',...
+            'classical (RS implementation - exploss)',...
+            'classical (CB implementation exploss)',...
+            'classical (CB implementation squaredloss)');
