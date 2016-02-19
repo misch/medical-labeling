992819e78f3f9f3e0d542ff8aaf14a1943269768
Michèle Wyss <michele_wyss@students.unibe.ch>
2016-02-19 16:10:20 +0100

    added calculation and drawing of 1 degree angle on image

--- matlab-code/calculate1degradius.m
+++ matlab-code/calculate1degradius.m
@@ -1,14 +1,37 @@
-% get 1° radius on image
+% This script puts a 1° radius on an image that has been shown on full
+% screen
+% test_img = im2double(imread('../data/Dataset8/input-frames/frame_00195.png'));
+img = im2double(imread('../data/Dataset2/input-frames/frame_00206.png'));
 
-% cm on screen: tan(1°) * distance_to_screen [cm]
+% These values are taken from http://dev.theeyetribe.com/general/
+insecurity = 1; % in degrees
+distance_to_screen = 60; % in cm
 
-% from recorded "display width" and "display height", take the one that is
-% unrealistic (i.e. bigger than actual screen size) and figure out how much
-% it has been reduced.
+% These values are screen-specific. With Ubuntu, the values can be found
+% using the following command in a terminal:
 %
-% e.g. if display height = 1900 and actual display resolution = 1920 x
-% 1080:
+%   xdpyinfo | grep dimensions
 %
-% x = 1900 / 1080 % wished height, if width were full-screen vs. actually possible size
-% y = 1920 / x % actual width, given that height is smaller than expected
+screen_width = 51.8; % in cm
+screen_height = 32.4; % in cm
 
+
+img_height = size(img,1);
+
+radius_on_screen = tan(deg2rad(insecurity)) * distance_to_screen; % in cm
+
+
+% find relation between full screen size and 1 degree radius
+width_relation = screen_width/radius_on_screen;
+height_relation = screen_height/radius_on_screen;
+
+
+middle = flip([size(img,1), size(img,2)])/2;
+
+% if video height matches screen height, use height-relation
+% if video width matches screen width, use width-relation
+pixel_radius_on_image = img_height/height_relation;
+
+
+imshow(img,'Border','tight');
+viscircles(middle, pixel_radius_on_image,'EnhanceVisibility',false);
