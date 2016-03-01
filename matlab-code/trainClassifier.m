9d073860fd463d3bf0fdec7f13179465ffda4783
Michèle Wyss <michele_wyss@students.unibe.ch>
2016-03-01 16:22:57 +0100

    updated parameters

--- matlab-code/trainClassifier.m
+++ matlab-code/trainClassifier.m
@@ -32,20 +32,23 @@ if strcmp(classifier,'svm')
         end
     else % Perform real training
         disp('Train SVM classifier...');
-        [train_data, train_labels] = getFiftyFiftySamples(train_data,train_labels);
+%         [train_data, train_labels] = getFiftyFiftySamples(train_data,train_labels);
+%         save('matlab.mat','train_data','train_labels');
+%         load('matlab.mat');
         model = libsvmtrain(train_labels,train_data,'-t 2 -g 0.0625 -c 10');
 %         model = fitcsvm(train_data, train_labels,'KernelFunction','RBF','KernelScale','auto');
     end
 elseif strcmp(classifier,'grad_boost')
    addpath('../sqb-0.1/build/');
-   options = struct(   'loss', 'squaredloss',...
+%     [train_data, train_labels] = getFiftyFiftySamples(train_data,train_labels);
+   options = struct(   'loss', 'exploss',...
                     'shrinkageFactor', 0.1,...
-                    'subsamplingFactor', 0.1,...
+                    'subsamplingFactor', 0.2,...
                     'maxTreeDepth', uint32(2),...
                     'disableLineSearch', uint32(0),...
                      'mtry',uint32(ceil(sqrt(size(train_data,2)))));
     disp('Train gradient boost classifier...');
-    model = SQBMatrixTrain( single(train_data), train_labels, uint32(5000), options);
+    model = SQBMatrixTrain( single(train_data), train_labels, uint32(50000), options);
 elseif strcmp(classifier,'pu_grad_boost')
     
     addpath('./pugradboost/');
@@ -59,9 +62,10 @@ elseif strcmp(classifier,'pu_grad_boost')
     f_distances = sqrt(sum((training_set.data - repmat(median(positives),size(training_set.data,1),1)).^2,2));
        
     prob = exp(-f_distances/0.15) .* exp(-s_distances/400);
+    
 %     load('prob_spatialAndColor');
 
-%     train_labels(train_labels == -1) = 0;
+    train_labels(train_labels == -1) = 0;
 
     [model,~] = learnPuboost(train_data,train_labels, prob,200);
 else
