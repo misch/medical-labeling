d34bc1479bd640cb1e856f5cf4e3ee31cf67de9e
Michèle Wyss <michele_wyss@students.unibe.ch>
2016-03-03 10:55:09 +0100

    readCSVFile.m can now handle starting at 1st or at 6th row

--- matlab-code/readCSVFile.m
+++ matlab-code/readCSVFile.m
@@ -1,11 +1,12 @@
 function framePositions = readCSVFile(filename);
 % This function reads the CSV-File from the gaze
-% tracker and returns the framePositions
-
-    fid = fopen(filename);
-    out = textscan(fid,'%f%f%f%f%f','delimiter',';');
-    fclose(fid);
+% tracker and returns the framePositions  
+    try
+        disp('Try reading from beginning on...');
+        out = my_csvread(filename,0,0,';');
+    catch
+        disp('Catch...');
+        out = my_csvread(filename,5,0,';');
+    end
     
-    % framePositions: [position1, position2, key_pressed]
-    framePositions = [out{4} out{5} out{3}];
-   
+    framePositions = [out(:,4) out(:,5) out(:,3)];
