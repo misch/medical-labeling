2363a9c50720dd12e4907995f88e8b0fbd152b77
Michèle Wyss <michele_wyss@students.unibe.ch>
2016-03-04 11:32:30 +0100

    added scaling to video writer because there were otherwise troubles with the tracking program..

--- matlab-code/writeFramesToVideo.m
+++ matlab-code/writeFramesToVideo.m
@@ -1,5 +1,5 @@
 function writeFramesToVideo(frames_dir, output_filename, frame_rate)
-    % Write a sequence of input frames to a single uncompressed video
+    % Write a sequence of input frames to a video.
 
     v = VideoWriter(output_filename);
 
@@ -8,7 +8,8 @@ function writeFramesToVideo(frames_dir, output_filename, frame_rate)
     file_names = dir([frames_dir, '*.png']);
     open(v)
     for file = file_names'
-        frame = im2double(imread(([frames_dir,file.name])));  
+        frame = im2double(imread([frames_dir,file.name]));
+        frame = imresize(frame,0.8,'nearest');
         writeVideo(v, frame);
     end
 
