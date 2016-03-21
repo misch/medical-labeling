037619a9335be31fa17a700bb123c1b4e2827d60
Michèle Wyss <michele_wyss@students.unibe.ch>
2016-03-21 12:41:26 +0100

    added another script to quickly plot ROC- and PR-curves
    will have to clean up at one point...

---
+++ matlab-code/visualizations/plotROCandPRcurve.m
@@ -0,0 +1,16 @@
+% 1st, load ROC.mat and PR.mat
+
+%% Plot ROC curve
+f_pr = figure(1);
+hold on; plot(recall,precision,'LineWidth',2);
+axis( [0 1 0 1] );
+xlabel('Recall','FontSize',16);
+ylabel('Precision','FontSize',16);
+
+%% Plot PR curve
+f_roc = figure(2);
+hold on;
+plot(false_positive_rate,recall,'LineWidth',2);
+axis( [0 1 0 1] );
+xlabel('False Positive Rate','FontSize',16);
+ylabel('True Positive Rate','FontSize',16);
