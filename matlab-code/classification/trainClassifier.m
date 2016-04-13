75e3e8a18d8b444d1d92303897ab25ab8d5f4765
Michele Wyss <michele_wyss@students.unibe.ch>
2016-04-13 14:59:09 +0000

    latest updates from trainClassifier.m

--- matlab-code/classification/trainClassifier.m
+++ matlab-code/classification/trainClassifier.m
@@ -33,19 +33,14 @@ if strcmp(classifier,'svm')
         end
     else % Perform real training
         disp('Train SVM classifier...');
-%         [train_data, train_labels] = getFiftyFiftySamples(train_data,train_labels);
-%         save('matlab.mat','train_data','train_labels');
-%         load('matlab.mat');
         model = libsvmtrain(train_labels,train_data,'-t 2 -g 0.0125 -c 0.5');
-%         model = fitcsvm(train_data, train_labels,'KernelFunction','RBF','KernelScale','auto');
     end
 elseif strcmp(classifier,'grad_boost')
    addpath('../sqb-0.1/build/');
-%     [train_data, train_labels] = getFiftyFiftySamples(train_data,train_labels);
    options = struct(   'loss', 'exploss',...
                     'shrinkageFactor', 0.1,...
                     'subsamplingFactor', 0.2,...
-                    'maxTreeDepth', uint32(2),...
+                    'maxTreeDepth', uint32(0),...
                     'disableLineSearch', uint32(0),...
                      'mtry',uint32(ceil(sqrt(size(train_data,2)))));
     disp('Train gradient boost classifier...');
@@ -56,24 +51,28 @@ elseif strcmp(classifier,'pu_grad_boost')
     s_distances = sqrt(sum((training_set.gaze_position - training_set.median_superpixel_pos).^2,2));
     
     
-    positive_idx = training_set.labels == 1;
+     positive_idx = training_set.labels == 1;
     positives = training_set.data(positive_idx,:);
-    
-    
-    % median of distances
-    if dataset==2
-        f_distances = median(pdist2(training_set.data,positives),2);
-        prob = exp(-f_distances/0.15) .* exp(-s_distances/400);
-    elseif dataset==7
-        f_distances = median(pdist2(training_set.data,positives,'cosine'),2);
-        prob = exp(-f_distances/0.15) .* exp(-s_distances/400);
-    elseif dataset==8
-        min_f_distances = min(pdist2(training_set.data,positives,'cosine'),[],2);
-        prob = exp(-(min_f_distances/max(min_f_distances(:)))/3).*exp(-s_distances/40);
-    end
 
-%     train_labels(train_labels == -1) = 0;
+    train_labels(train_labels == -1) = 0;
 %     train_labels(s_distances<30) = 1;
+
+    if sum(train_labels==0) == 0 % tell that there are no unlabeled samples
+        disp('There were no unlabeled samples. Standard exponential loss will be optimized.');
+        prob = zeros(size(train_labels,1),1);
+    else % calculate probabilities for the unlabeled samples
+        if dataset==2
+            f_distances = median(pdist2(training_set.data,positives),2);
+            prob = exp(-f_distances/0.15) .* exp(-s_distances/400);
+        elseif dataset==7
+            f_distances = median(pdist2(training_set.data,positives,'cosine'),2);
+            prob = exp(-f_distances/0.35) .* exp(-s_distances/5);
+        elseif dataset==8
+            min_f_distances = min(pdist2(training_set.data,positives,'cosine'),[],2);
+%             prob = exp(-(min_f_distances/max(min_f_distances(:)))/3).*exp(-s_distances/40);
+            prob = exp(-(min_f_distances/max(min_f_distances(:)))/1.5).*exp(-s_distances/5);
+        end
+    end
 
     [model,info] = learnPuboost(train_data,train_labels, prob,500);
     figure(); plot(1:length(info.LOSS),info.LOSS); title('loss');
