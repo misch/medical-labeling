0c3543c56d98189ccf7524c71dc10e2e7b7df6f7
Michèle Wyss <michele_wyss@students.unibe.ch>
2016-04-14 12:20:03 +0200

    added more detailed description to main script

--- matlab-code/main.m
+++ matlab-code/main.m
@@ -1,36 +1,57 @@
-%% Add subfolders
+% This is the main script of the project to use eye-tracking for labeling
+% in 3D. It performs the following steps:
+%
+% 1) add subfolders: this should always be executed, even if only other
+% parts of the code are being used, as otherwise the necessary matlab
+% functions might not be found.
+%
+% 2) prepare data: this part is used when a new dataset is being used; that
+% is, a completely new video or 3D volume. It will write the data to image
+% files and create some descriptors for them. Also, whenever new
+% descriptors for already existing datasets should be generated (e.g. to
+% try out new features), the prepareData-script should be executed.
+%
+% 3) fix dataset: as many functions will use the dataset to automatically
+% determine where to find things (see file structure in README.md), this
+% usually should be executed.
+%
+% 4) assemble training data: this part has to be executed, if the training
+% data to be used does not yet exist (e.g. right after generating the
+% descriptors in step 2), step 4) is necessary to collect from all the
+% superpixels the positives and negatives using the gaze data
+%
+% 5) train classifier: train a classifier
+%
+% 6) test classifier: test the classifier on some chosen frames
+% (performance curves will be calculated pixel-wise, even though all the
+% pixels of one superpixel will get the same scores)
+%% 1) add subfolders
 addpath(genpath(pwd));
 
 
-%%
+%% 2) prepare data
 run prepareData
-%%
+
+%% 3) fix dataset
 dataset = 2;
-%%
+
+%% 4) assemble training data
+
 %assembleTrainingDataSuperpixels(dataset,'trainingAutoencodedSuperpixels.mat');
 
 observations = dir(['../data/Dataset',num2str(dataset),'/gaze-measurements/*.csv']);
 for ii = 1:length(observations)
     assembleTrainingDataSuperpixels(dataset,['trainingSmallSuperpixelsCoocc_new',num2str(ii),'.mat'],observations(ii).name);
 end
-%%
-assembleReferenceTrainingDataSuperpixels(dataset,'trainingSmallSuperpixelsCOOCCnew_onePositivePerFrame-0_5.mat','../data/Dataset8/ground_truth-frames/',true);
-% for reference: key_pressed values of:
-% - d2: gaze observation #2
-% - d7: gaze observation #2
-% - d8: gaze observation #2
-%%
-assembleTrainingDataPatches(dataset,'trainingPatches1.mat');
-%%
+
+% assembleReferenceTrainingDataSuperpixels(dataset,'trainingSmallSuperpixelsCOOCCnew_onePositivePerFrame-0_5.mat','../data/Dataset8/ground_truth-frames/',true);
+% assembleTrainingDataPatches(dataset,'trainingPatches1.mat');
+
+%% 5) train classifier
 classifier = 'pu_grad_boost';
 model = trainClassifier(dataset, classifier, 'trainingSuperpixelsColorReference-0_5');
 
-%%
-testSuperpixelClassifier(model, dataset, [139:10:229], classifier,'small-superpixels-coocc-descriptors_new/'); %find(~key_pressed)'
-% testPatchClassifier(model,dataset,[105:50:655], classifier);
-
-% frames to test:
-% Dataset2: [105:50:655]
-% Dataset5: [9:30:279]
-% Dataset7: [30:3:60]
-% Dataset8: [139:10:229] (100-230)
+%% 6) test classifier
+test_frames = [139:10:229];
+testSuperpixelClassifier(model, dataset, test_frames, classifier,'small-superpixels-coocc-descriptors_new/');
+% testPatchClassifier(model,dataset,test_frames, classifier);
