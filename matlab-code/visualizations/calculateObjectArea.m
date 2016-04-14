a07135957b519ef3904557fab0eb110f95154a8a
Michèle Wyss <michele_wyss@students.unibe.ch>
2016-04-14 11:59:50 +0200

    (re)moved some scripts

--- matlab-code/visualizations/calculateObjectArea.m
+++ matlab-code/visualizations/calculateObjectArea.m
@@ -1,3 +1,5 @@
+% Calculate and plot the area of an object throughout the whole dataset
+% (all the frames)
 clc;
 dataset = 8;
 
@@ -18,34 +20,17 @@ avg_area = mean(area(area>0))
 relative_avg_area = avg_area / (frame_width*frame_height)
 %%
 
-% subplot(2,1,1);
 f = figure; hold on;
-% plot(area/(frame_width*frame_height),'k','LineWidth',3); axis([1 1125 0 0.2]);
 plot(area,'b','LineWidth',3); xlim([1 length(file_names)]);
 hold on; plot(1:length(file_names),avg_area*ones(1,length(file_names)),'b--','LineWidth',2);
-% title('eye tumor size','FontSize',18,'FontWeight','bold');
+
 xlabel('frame','FontSize',18,'FontWeight','bold');
 ylabel('object size [#px]','FontSize',18,'FontWeight','bold');
 box off;
 
-saveToPDFWithoutMargins(f,'test.pdf');
-% legendies(1) = plot(NaN,NaN,'b','LineWidth',3);
-% legendies(2) = plot(NaN,NaN,'r','LineWidth',3);
-% legendies(3) = plot(NaN,NaN,'k','LineWidth',3); % black --> not on object
-% le = legend(legendies,'instrument','eye tumor', 'cochlea','Location','north');
-% le.FontSize = 18;
-% saveToPDFWithoutMargins(f,'size_instrument.pdf');
-% subplot(2,1,2);
-% 
-% title('% positives of frame');
-
-%%
-f2 = figure; hold on;
-% plot(area/(frame_width*frame_height),'k','LineWidth',3); axis([1 1125 0 0.2]);
-plot(gradient(area),'b','LineWidth',3); xlim([1 length(file_names)]);
-% title('eye tumor size','FontSize',18,'FontWeight','bold');
+hold on; plot(gradient(area),'LineWidth',1); xlim([1 length(file_names)]);
 xlabel('frame','FontSize',18,'FontWeight','bold');
 ylabel('object size [#px]','FontSize',18,'FontWeight','bold');
 box off;
-
-saveToPDFWithoutMargins(f,'test.pdf');
+legend('object area','mean','gradient');
+% saveToPDFWithoutMargins(f,'test.pdf');
